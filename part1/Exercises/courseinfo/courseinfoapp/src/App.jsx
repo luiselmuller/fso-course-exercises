c4381826a3e1d66d467373429bc58dddd6d60aea
luiselmuller <luiselmuller@gmail.com>
2025-01-11 19:57:39 -0400

    Completed 1.3: Course Information, step 3
    Changed variable definitions to use objects and refactored code to make it work

--- part1/Exercises/courseinfo/courseinfoapp/src/App.jsx
+++ part1/Exercises/courseinfo/courseinfoapp/src/App.jsx
@@ -11,13 +11,13 @@ const Content = (props) => {
   return (
     <div>
       <p>
-        {props.parts[0]}: {props.exercises[0]}
+        {props.parts[0].name}: {props.parts[0].exercises}
       </p>
       <p>
-        {props.parts[1]}: {props.exercises[1]}
+        {props.parts[1].name}: {props.parts[1].exercises}
       </p>
       <p>
-        {props.parts[2]}: {props.exercises[2]}
+        {props.parts[2].name}: {props.parts[2].exercises}
       </p>
     </div>
   )
@@ -33,18 +33,24 @@ const Total = (props) => {
 
 const App = () => {
   const course = 'Half Stack application development'
-  const part1 = 'Fundamentals of React'
-  const exercises1 = 10
-  const part2 = 'Using props to pass data'
-  const exercises2 = 7
-  const part3 = 'State of a component'
-  const exercises3 = 14
+  const part1 = {
+    name: 'Fundamentals of React',
+    exercises: 10
+  }
+  const part2 = {
+    name: 'Using props to pass data',
+    exercises: 7
+  }
+  const part3 = {
+    name: 'State of a component',
+    exercises: 14
+  }
 
   return (
     <div>
       <Header course={course} />
-      <Content parts={[part1, part2, part3]} exercises={[exercises1, exercises2, exercises3]} />
-      <Total total={[exercises1, exercises2, exercises3]} />
+      <Content parts={[part1, part2, part3]} />
+      <Total total={[part1.exercises, part2.exercises, part3.exercises]} />
     </div>
   )
 }
