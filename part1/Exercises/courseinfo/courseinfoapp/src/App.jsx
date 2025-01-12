f88391897979b2d2457709ae273ff518f951d164
luiselmuller <luiselmuller@gmail.com>
2025-01-11 20:12:57 -0400

    Completed 1.5: Course Information step 5
    Changed the variables to be one single object and fixed everything that broke

--- part1/Exercises/courseinfo/courseinfoapp/src/App.jsx
+++ part1/Exercises/courseinfo/courseinfoapp/src/App.jsx
@@ -2,7 +2,7 @@
 const Header = (props) => {
   return (
     <div>
-      <h1>{props.course}</h1>
+      <h1>{props.course.name}</h1>
     </div>
   )
 }
@@ -10,7 +10,7 @@ const Header = (props) => {
 const Content = (props) => {
   return (
     <div>
-      {props.parts.map((part, index) => {
+      {props.course.parts.map((part, index) => {
         return (
             <p key={index}>{part.name} - {part.exercises}</p>
         )
@@ -22,33 +22,35 @@ const Content = (props) => {
 const Total = (props) => {
   return (
     <div>
-      <p>Total exercises: {props.parts.reduce((acc, part) => acc + part.exercises, 0)}</p>
+      <p>Total exercises: {props.course.parts.reduce((acc, part) => acc + part.exercises, 0)}</p>
     </div>
   )
 }
 
 const App = () => {
-  const course = 'Half Stack application development'
-  const parts = [
-    {
-      name: 'Fundamentals of React',
-      exercises: 10
-    },
-    {
-      name: 'Using props to pass data',
-      exercises: 7
-    },
-    {
-      name: 'State of a component',
-      exercises: 14
-    }
-  ]
+  const course = {
+    name: 'Half Stack application development',
+    parts: [
+      {
+        name: 'Fundamentals of React',
+        exercises: 10
+      },
+      {
+        name: 'Using props to pass data',
+        exercises: 7
+      },
+      {
+        name: 'State of a component',
+        exercises: 14
+      }
+    ]
+  }
 
   return (
     <div>
       <Header course={course} />
-      <Content parts={parts} />
-      <Total parts={parts} />
+      <Content course={course} />
+      <Total course={course} />
     </div>
   )
 }
