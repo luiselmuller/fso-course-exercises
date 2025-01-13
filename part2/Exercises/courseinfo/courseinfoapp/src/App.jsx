c69fbd5a5ee6e818594de5cc77eaaec8300030bf
luiselmuller <luiselmuller@gmail.com>
2025-01-13 15:15:12 -0400

    2.1: Course information step 6

---
+++ part2/Exercises/courseinfo/courseinfoapp/src/App.jsx
@@ -0,0 +1,66 @@
+/* eslint-disable react/prop-types */
+const Header = ({ course }) => {
+  return (
+    <div>
+      <h1>{course.name}</h1>
+    </div>
+  )
+}
+
+const Content = ({ course: { parts } }) => {
+  return (
+    <div>
+      {parts.map((part, index) => {
+        return (
+            <p key={index}>{part.name} - {part.exercises}</p>
+        )
+      })}
+    </div>
+  )
+}
+
+const Total = ({ course: { parts } }) => {
+  return (
+    <div>
+      <p>Total exercises: {parts.reduce((acc, part) => acc + part.exercises, 0)}</p>
+    </div>
+  )
+}
+
+const Course = ({ course }) => {
+  return (
+    <div>
+      <Header course={course} />
+      <Content course={course} />
+      <Total course={course} />
+    </div>
+  )
+}
+
+const App = () => {
+  const course = {
+    id: 1,
+    name: 'Half Stack application development',
+    parts: [
+      {
+        name: 'Fundamentals of React',
+        exercises: 10,
+        id: 1
+      },
+      {
+        name: 'Using props to pass data',
+        exercises: 7,
+        id: 2
+      },
+      {
+        name: 'State of a component',
+        exercises: 14,
+        id: 3
+      }
+    ]
+  }
+
+  return <Course course={course} />
+}
+
+export default App
