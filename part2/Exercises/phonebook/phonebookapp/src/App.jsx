2377d00e39312d83bde7e71cf3377e7c72dccbbf
luiselmuller <luiselmuller@gmail.com>
2025-01-13 16:23:29 -0400

    Completed 2.9*: The Phonebook Step 4

--- part2/Exercises/phonebook/phonebookapp/src/App.jsx
+++ part2/Exercises/phonebook/phonebookapp/src/App.jsx
@@ -2,20 +2,26 @@ import { useState } from 'react'
 
 const App = () => {
   const [persons, setPersons] = useState([
-    { name: 'Arto Hellas', number: '39-44-5323523' }
+    { name: 'Arto Hellas', number: '040-123456', id: 1 },
+    { name: 'Ada Lovelace', number: '39-44-5323523', id: 2 },
+    { name: 'Dan Abramov', number: '12-43-234345', id: 3 },
+    { name: 'Mary Poppendieck', number: '39-23-6423122', id: 4 }
   ]) 
   const [newName, setNewName] = useState('')
   const [newNumber, setNewNumber] = useState('')
+  const [filter, setFilter] = useState('')
 
   const addPerson = (event) => {
     event.preventDefault()
     if (persons.some(person => person.name === newName || person.number === newNumber)) {
       alert(`${newName} or ${newNumber} is already in the phonebook`)
       setNewName('')
+      setNewNumber('')
     }
     else {
       setPersons(persons.concat({ name: newName, number: newNumber }))
       setNewName('')
+      setNewNumber('')
     }
   }
 
@@ -27,9 +33,19 @@ const App = () => {
     setNewNumber(event.target.value)
   }
 
+  const onFilterChange = (event) => {
+    setFilter(event.target.value)
+  }
+
   return (
     <div>
       <h2>Phonebook</h2>
+      <p>
+        filter shown with
+      </p>
+      <input type="text" onChange={onFilterChange} />
+
+      <h3>Add a new person</h3>
       <form onSubmit={addPerson}>
         <div>
           name: <input value={newName} onChange={onNameChange} />
@@ -40,10 +56,12 @@ const App = () => {
         </div>
       </form>
       <h2>Numbers</h2>
-      {persons.map(person => (
-        <div key={person.name}>
-          {person.name}: {person.number}
-        </div>
+      {persons.filter(person => (
+        person.name.toLowerCase().includes(filter.toLowerCase()) || person.number.includes(filter)
+      )).map(person => (
+        <p key={person.id}>
+          {person.name} {person.number}
+        </p>
       ))}
     </div>
   )
