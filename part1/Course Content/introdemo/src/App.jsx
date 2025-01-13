ae935f3d52052f93ea19cf6e37b5c6204f4f3799
luiselmuller <luiselmuller@gmail.com>
2025-01-12 20:22:56 -0400

    Course Content

--- part1/Course Content/introdemo/src/App.jsx
+++ part1/Course Content/introdemo/src/App.jsx
@@ -1,31 +1,70 @@
 /* eslint-disable react/prop-types */
-const Hello = (props) => {
-  console.log(props)
-  return (
-    <div>
-      <p>Hello {props.name}, you are {props.age} years old</p>
-    </div>
-  )
-}
+import { useState } from 'react'
+// const Hello = ({ name, age }) => {
+//   const bornYear = () => new Date().getFullYear() - age
+
+//   return (
+//     <div>
+//       <p>Hello {name}, you are {age} years old</p>
+//       <p>You were probably born in {bornYear()}</p>
+//     </div>
+//   )
+// }
+
+// const App = () => {
+//   const now = new Date()
+//   const a = 10
+//   const b = 20
+//   const name = "Peter"
+//   console.log(now, a+b)
+  
+//   return (
+//     <div>
+//       <h1>Hello World, it is {now.toString()}</h1>
+//       <p>
+//         {a} plus {b} is {a + b}
+//       </p>
+
+//       <Hello name="John" age={30} />
+//       <Hello name={name} age={a + b} />
+//     </div>
+//   )
+// }
+const Display = ({ counter }) => <div>{counter}</div>
+
+const Button = ({ onSmash, text }) => <button onClick={onSmash}>{text}</button>
 
 const App = () => {
-  const now = new Date()
-  const a = 10
-  const b = 20
-  const name = "Peter"
-  console.log(now, a+b)
-  
+  const [counter, setCounter] = useState(0)
+
+  console.log('rendering with counter value', counter)
+
+  const increaseByOne = () => {
+
+    console.log('increasing, value before', counter)
+    setCounter(counter + 1)
+  }
+
+  const decreaseByOne = () => { 
+
+    console.log('decreasing, value before', counter)
+    setCounter(counter - 1)
+  }
+
+  const setToZero = () => {
+
+    console.log('resetting to zero, value before', counter)
+    setCounter(0)
+  }
+
   return (
     <div>
-      <h1>Hello World, it is {now.toString()}</h1>
-      <p>
-        {a} plus {b} is {a + b}
-      </p>
-
-      <Hello name="John" age={30} />
-      <Hello name={name} age={a + b} />
+      <Display counter={counter} />
+      <Button onClick={increaseByOne} text="plus" />
+      <Button onClick={setToZero} text="zero" />
+      <Button onClick={decreaseByOne} text="minus" />
     </div>
   )
-}
+} 
 
 export default App
