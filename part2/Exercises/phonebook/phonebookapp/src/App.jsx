45efdcad7ed179aa66e94ec92b2be2dedc12d0a7
luiselmuller <luiselmuller@gmail.com>
2025-01-14 00:24:51 -0400

    Completed 2.12: The Phonebook step 7

--- part2/Exercises/phonebook/phonebookapp/src/App.jsx
+++ part2/Exercises/phonebook/phonebookapp/src/App.jsx
@@ -4,35 +4,43 @@ import PersonForm from './components/PersonForm'
 import Persons from './components/Persons'
 import axios from 'axios'
 
+
 const App = () => {
+  const [persons, setPersons] = useState([]) 
+  const [filter, setFilter] = useState('')
+  const [newName, setNewName] = useState('')
+  const [newNumber, setNewNumber] = useState('')
+
   useEffect(() => {
     axios.get('http://localhost:3001/persons')
          .then(response => setPersons(response.data))
          .catch(error => console.log(error))
   }, [])
 
-  const [persons, setPersons] = useState([]) 
+  useEffect(() => {
 
-  const [filter, setFilter] = useState('')
-  const [newName, setNewName] = useState('')
-  const [newNumber, setNewNumber] = useState('')
 
-  const onFilterChange = (event) => {
-    setFilter(event.target.value)
-  }
+  }, [newName, newNumber, persons])
 
   const addPerson = (event) => {
     event.preventDefault()
     if (persons.some(person => person.name === newName || person.number === newNumber)) {
-        alert(`${newName} or ${newNumber} is already in the phonebook`)
-        setNewName('')
-        setNewNumber('')
+      alert(`${newName} or ${newNumber} is already in the phonebook`)
+      setNewName('')
+      setNewNumber('')
     }
     else {
-        setPersons(persons.concat({ name: newName, number: newNumber, id: persons.length + 1 }))
+        axios.post('http://localhost:3001/persons', {name: newName, number: newNumber,  id: persons.length + 1 })
+        .then(response => setPersons(persons.concat(response.data)))
+        .catch(error => console.log(error))
         setNewName('')
         setNewNumber('')
     }
+
+  }
+
+  const onFilterChange = (event) => {
+    setFilter(event.target.value)
   }
 
   const onNameChange = (event) => {
