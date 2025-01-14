6fdbec2dbb3eeac53bf122b05a383b54083339f8
luiselmuller <luiselmuller@gmail.com>
2025-01-14 17:47:29 -0400

    Completed 2.18* Data for countries, step 1
    You can search and use the functionality outlined in the exercise but has some bugs that I'll probably fix with the other ones. (I also need to separate everything in modules) did it this way first to get it working

--- part2/Exercises/countrydata/countrydataapp/src/App.jsx
+++ part2/Exercises/countrydata/countrydataapp/src/App.jsx
@@ -6,16 +6,22 @@ const App = () => {
   const [countries, setCountries] = useState([])
 
   useEffect(() => {
-    axios.get(`https://studies.cs.helsinki.fi/restcountries/api/all`)
+    if(country) {
+      axios.get(`https://studies.cs.helsinki.fi/restcountries/api/all`)
       .then(res => {
         const filteredCountries = res.data.filter(c => c.name.common.toLowerCase().includes(country.toLowerCase()))
         setCountries(filteredCountries)
       })
       .catch(err => console.log(err))
+    }
+    else {
+      setCountries([])
+    }
   }, [country])
 
 
   const onCountryChange = (e) => {
+    console.log(e.target.value)
     setCountry(e.target.value)
   }
 
@@ -24,7 +30,31 @@ const App = () => {
       <p>find countries</p>
       <input type="text" onChange={onCountryChange} placeholder="search" />
       <ul>
-        {countries.length > 10 ? <p>Too many matches, specify another filter</p> : countries.map(country => <li key={country.name.common}>{country.name.common}</li>)}
+        {countries.length > 10 ? 
+          <p>There are {countries.length} countries matching {country}, specify another filter</p> :
+          countries.length === 1 ? countries.map(country => {
+            return (
+              <div key={country.name.common}>
+                <p>{country.name.common}</p>
+                <p>area: {country.area}</p>
+
+                <p>Languages: </p>
+                <ul>
+                  {
+                    Object.entries(country.languages).map(([key, value]) => 
+                      <li key={key}>{value}</li>
+                    )
+                  }
+                </ul>
+                <img src={country.flags.png} alt={country.name.common} />
+              </div>
+            )
+          }) : 
+          countries.map(country => 
+            <div key={country.name.common}>
+              <p>{country.name.common}</p>
+            </div>
+          )}
       </ul>
       
     </div>
